18569dd6360ed50dec6bf448b5712ec9b739db33
felippemozer <felippemozer22@gmail.com>
2023-12-12 22:02:01 -0300

    feat: setup network on hardhat config

--- x-smart-contracts/hardhat.config.js
+++ x-smart-contracts/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    staging: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/YoV6A1KNKtIJ9OCj0JW9FoUjCYk2mA6X",
+      accounts: [process.env.ACCOUNT_PK]
+    }
+  }
 };
